a9ae8a85b9429f9c094a6113c013dc8399424805
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-06 18:48:37 -0500

    S8-97 Creando los módulos y los componentes básicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PaisModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
