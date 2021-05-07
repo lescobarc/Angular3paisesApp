023e0b8b2c68b47be064d7b70e2f3138a3aecbb1
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-06 19:52:38 -0500

    S9-101 Style component sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styles:[
+     `li{
+       cursor: pointer;
+     }`
+  ],
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
