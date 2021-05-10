02be277a76a669fd107ffd331400047a3ddcd2a1
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-10 17:27:12 -0500

    S9-124 Mostrar sugerencias al escribir - autocomplete

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,13 +5,21 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+  styleUrls: ['./por-pais.component.css'],
+  styles:[
+    `li{
+      cursor: pointer`
+  ]
 })
 export class PorPaisComponent {
 
   termino: string = "";
   hayError: boolean = false;
   paises: Country[] = [];
+
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
+
   constructor(private paisService: PaisService) { }
 
   buscar( termino: string ) {
@@ -31,6 +39,21 @@ export class PorPaisComponent {
   }
   sugerencias( termino: string){
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais(termino)
+     .subscribe( 
+       paises => {
+         this.paisesSugeridos = paises.splice(0,5);
+       },
+       (error) => this.paisesSugeridos = []
+       );
+    
+  }
+
+  buscarSugerido( termino: string ){
+    this.buscar( termino );
     
   }
 
